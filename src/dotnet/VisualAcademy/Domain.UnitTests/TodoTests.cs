d18deb4cd81c0ecfead7a0ac7e991d856cb9f19f
Park YongJune <redplus@redplus.net>
2022-12-29 05:43:32 +0900

    SetUp 특성으로 테스트 클래스에 필요한 개체를 미리 준비 후 테스트하기

--- src/dotnet/VisualAcademy/Domain.UnitTests/TodoTests.cs
+++ src/dotnet/VisualAcademy/Domain.UnitTests/TodoTests.cs
@@ -6,11 +6,36 @@ namespace Domain.UnitTests
     [TestFixture]
     public class TodoTests
     {
+        #region TestGetIsComplete
         [Test]
         public void TestGetIsComplete()
         {
             var sut = new Todo();
             Assert.That(sut.IsComplete, Is.False);
+        }
+        #endregion
+        private Todo _todo = null!;
+        private const int Id = 1;
+        private const string Name = "Domain Test";
+
+        [SetUp]
+        public void SetUp() 
+        { 
+            _todo = new Todo();
+        }
+
+        [Test]
+        public void TestSetAndGetId()
+        {
+            _todo.Id = Id;
+            Assert.That(_todo.Id, Is.EqualTo(Id));
+        }
+
+        [Test]
+        public void TestSetAndGetName()
+        {
+            _todo.Name = Name;
+            Assert.That(_todo.Name, Is.EqualTo(Name));
         }
     }
 }
