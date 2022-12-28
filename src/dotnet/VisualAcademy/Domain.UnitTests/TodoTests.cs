fa23d7c54fb1740c9c597de047c6bfb1348f368b
Park YongJune <redplus@redplus.net>
2022-12-29 05:27:01 +0900

    TodoTests 이름으로 테스트 클래스 만들고 IsComplete 속성 기본값 테스트

---
+++ src/dotnet/VisualAcademy/Domain.UnitTests/TodoTests.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entities;
+using NUnit.Framework;
+
+namespace Domain.UnitTests
+{
+    [TestFixture]
+    public class TodoTests
+    {
+        [Test]
+        public void TestGetIsComplete()
+        {
+            var sut = new Todo();
+            Assert.That(sut.IsComplete, Is.False);
+        }
+    }
+}
