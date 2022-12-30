baad75bd05db908ac02896fff8d934f639a7d950
Park YongJune <redplus@redplus.net>
2022-12-30 11:41:50 +0900

    Persistence 프로젝트에 DatabaseService 클래스 생성 및 코드 구현

---
+++ src/dotnet/VisualAcademy/Persistence/DatabaseService.cs
@@ -0,0 +1,22 @@
+﻿using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence
+{
+    public class DatabaseService : DbContext, IDatabaseService
+    {
+        public DatabaseService(DbContextOptions<DatabaseService> options) 
+            : base(options)
+        { 
+        
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+
+        public void Save()
+        {
+            this.SaveChanges(); 
+        }
+    }
+}
