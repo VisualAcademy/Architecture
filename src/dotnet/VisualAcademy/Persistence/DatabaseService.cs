22d38a7f14b60342759bc53369c42b44a2067327
Park YongJune <redplus@redplus.net>
2022-12-30 12:41:48 +0900

    Presentation 프로젝트에서 인메모리를 사용하여 데이터 출력하기

--- src/dotnet/VisualAcademy/Persistence/DatabaseService.cs
+++ src/dotnet/VisualAcademy/Persistence/DatabaseService.cs
@@ -6,17 +6,21 @@ namespace Persistence
 {
     public class DatabaseService : DbContext, IDatabaseService
     {
-        public DatabaseService(DbContextOptions<DatabaseService> options) 
-            : base(options)
-        { 
-        
-        }
+        public DatabaseService(DbContextOptions<DatabaseService> options)
+            : base(options) => Database.EnsureCreated();
 
         public DbSet<Todo> Todos { get; set; } = null!;
 
-        public void Save()
+        public void Save() => this.SaveChanges();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            this.SaveChanges(); 
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Todo>().HasData(
+                new Todo { Id = 1, Name = "Domain", IsComplete = true },
+                new Todo { Id = 2, Name = "Application", IsComplete = true },
+                new Todo { Id = 3, Name = "Persistence", IsComplete = true });
         }
     }
 }
--- src/dotnet/VisualAcademy/Presentation/Program.cs
+++ src/dotnet/VisualAcademy/Presentation/Program.cs
@@ -1,6 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<DatabaseService>(opt => opt.UseInMemoryDatabase("TodoDb"));
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+//[1] DbContext 클래스 사용
+app.MapGet("/api/todos/withdbcontext", (DatabaseService db) => db.Todos.ToList());
 
 app.Run();
