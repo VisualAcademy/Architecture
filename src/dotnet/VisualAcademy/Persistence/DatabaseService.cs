edfc3f758a8fad561dcca57d00e0b7c754abf065
Park YongJune <redplus@redplus.net>
2023-01-03 20:15:15 +0900

    Convert to file-scoped namespace

--- src/dotnet/VisualAcademy/Persistence/DatabaseService.cs
+++ src/dotnet/VisualAcademy/Persistence/DatabaseService.cs
@@ -3,26 +3,25 @@ using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence.Todos;
 
-namespace Persistence
+namespace Persistence;
+
+public class DatabaseService : DbContext, IDatabaseService
 {
-    public class DatabaseService : DbContext, IDatabaseService
+    public DatabaseService(DbContextOptions<DatabaseService> options)
+        : base(options) => Database.EnsureCreated();
+
+    public DbSet<Todo> Todos { get; set; } = null!;
+
+    public void Save() => this.SaveChanges();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        public DatabaseService(DbContextOptions<DatabaseService> options)
-            : base(options) => Database.EnsureCreated();
+        base.OnModelCreating(modelBuilder);
 
-        public DbSet<Todo> Todos { get; set; } = null!;
-
-        public void Save() => this.SaveChanges();
-
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-
-            //modelBuilder.Entity<Todo>().HasData(
-            //    new Todo { Id = 1, Name = "Domain", IsComplete = true },
-            //    new Todo { Id = 2, Name = "Application", IsComplete = true },
-            //    new Todo { Id = 3, Name = "Persistence", IsComplete = true });
-            new TodoConfiguration().Configure(modelBuilder.Entity<Todo>());
-        }
+        //modelBuilder.Entity<Todo>().HasData(
+        //    new Todo { Id = 1, Name = "Domain", IsComplete = true },
+        //    new Todo { Id = 2, Name = "Application", IsComplete = true },
+        //    new Todo { Id = 3, Name = "Persistence", IsComplete = true });
+        new TodoConfiguration().Configure(modelBuilder.Entity<Todo>());
     }
 }
